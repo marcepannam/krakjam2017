49f53e06d9a0f253cb478f1d6fefcf2a827f7755
Michał Zieliński <michal@zielinscy.org.pl>
2017-01-22 10:03:53 +0100

    initial support for broken platforms
    Former-commit-id: e901a36f1e98fbf7c0fd98f33a7e76e71ff537a6

--- game/dziadek.gd
+++ game/dziadek.gd
@@ -36,6 +36,8 @@ var menu_scene = preload("res://menu.tscn")
 var staff_end
 var shoulder_ref
 
+signal platform_changed
+
 func _ready():
 	set_process(true)
 	set_process_input(true)
@@ -52,6 +54,7 @@ func _ready():
 	
 func start_waiting():
 	print("ready ", get_global_pos())
+	emit_signal("platform_changed", target_platform)
 	animation_player.play("stand")
 	state = AIMING
 	# Should we rotate? Count platforms on the right and above.
